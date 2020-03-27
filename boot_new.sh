e7962c6c679efcdd7b8128ddc01ec7e8a0c3deba
Alex Merenda <alexm@moove.ai>
2020-03-27 18:23:38 -0400

    only install qgis via wget

--- boot_new.sh
+++ boot_new.sh
@@ -9,15 +9,15 @@ bash /usr/local/share/google/dataproc/bdutil/components/activate/anaconda.sh
 source /etc/profile.d/effective-python.sh
 source /etc/profile.d/conda.sh
 
-conda update -n base -c defaults conda
+#conda update -n base -c defaults conda
 
 conda install python==3.6.8
 
 wget -q https://anaconda.org/conda-forge/qgis/3.12.1/download/linux-64/qgis-3.12.1-py36h77e4444_2.tar.bz2 -O /tmp/qgis-3.12.1-py36h77e4444_2.tar.bz2
 conda install --offline --use-local /tmp/qgis-3.8.3-py36hee8cbbe_0.tar.bz2
 
-wget -q https://anaconda.org/conda-forge/libspatialindex/1.9.3/download/win-64/libspatialindex-1.9.3-he025d50_3.tar.bz2 -O /tmp/libspatialindex-1.9.3-he025d50_3.tar.bz2
-conda install --offline --use-local /tmp/libspatialindex-1.9.3-he025d50_3.tar.bz2
+#wget -q https://anaconda.org/conda-forge/libspatialindex/1.9.3/download/win-64/libspatialindex-1.9.3-he025d50_3.tar.bz2 -O /tmp/libspatialindex-1.9.3-he025d50_3.tar.bz2
+#conda install --offline --use-local /tmp/libspatialindex-1.9.3-he025d50_3.tar.bz2
 
 conda install jupyterlab
 echo "source /etc/profile.d/effective-python.sh" >> /etc/bash.bashrc
@@ -37,31 +37,31 @@ echo "setting python path environment vars"
 echo 'export LD_LIBRARY_PATH=/opt/conda/anaconda/lib' >> /etc/bash.bashrc
 echo 'export PYTHONPATH=/opt/conda/anaconda/share/qgis/python' >> /etc/bash.bashrc
 
-echo "cloning moove datascience repo"
-git clone https://GITHUB_OAUTH_TOKEN@github.com/moove-ai/moove-data-exploration.git
-cd moove-data-exploration
-git checkout feture-branch-panel-data-set
-
-
-echo "pip installation"
-#pip install msgpack  --upgrade --ignore-installed
-#pip install wrapt  --upgrade --ignore-installed
-#pip install -r ./requirements.txt --upgrade --ignore-installed
-
-# ensure pip is missing. This command should be run three times
-echo "ensure pip numpy is missing"
-pip uninstall -y numpy
-pip uninstall -y numpy
-pip uninstall -y numpy
-
-echo "ensure conda pip is installed"
-conda install numpy
-
-echo "ensure gdal is installed"
-conda install gdal
-
-echo "enusre urbanaccess is installed"
-pip install -U urbanaccess
+#echo "cloning moove datascience repo"
+#git clone https://a25d5effbb396932d8adf29615b15c26ccc4bc6f@github.com/moove-ai/moove-data-exploration.git
+#cd moove-data-exploration
+#git checkout feture-branch-panel-data-set
+#
+#
+#echo "pip installation"
+##pip install msgpack  --upgrade --ignore-installed
+##pip install wrapt  --upgrade --ignore-installed
+##pip install -r ./requirements.txt --upgrade --ignore-installed
+#
+## ensure pip is missing. This command should be run three times
+#echo "ensure pip numpy is missing"
+#pip uninstall -y numpy
+#pip uninstall -y numpy
+#pip uninstall -y numpy
+#
+#echo "ensure conda pip is installed"
+#conda install numpy
+#
+#echo "ensure gdal is installed"
+#conda install gdal
+#
+#echo "enusre urbanaccess is installed"
+#pip install -U urbanaccess
 
 
 echo "updating python path again"
