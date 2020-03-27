090fff90075ba291b6d1570c37df6413288b41ee
Alex Merenda <alexm@moove.ai>
2020-03-27 18:15:25 -0400

    annaconda-dataproc-18: qgis only

---
+++ createImage-qgis-only.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#sed -i '.bak' "s/GITHUB_OAUTH_TOKEN/$GITHUB_OAUTH_TOKEN/g" boot_new.sh
+
+python generate_custom_image.py \
+--image-name dataproc-custom-1-4-5-anaconda-$(date +%Y%m%d%H%M) \
+--dataproc-version 1.4.25-debian9 \
+--customization-script $(pwd)/boot_new_qgis_conda_forge_only.sh \
+--zone us-central1-b \
+--gcs-bucket gs://moove-dataproc-custom \
+--disk-size 100  \
+--machine-type n1-standard-8
+
+#git checkout -- boot_new.sh
