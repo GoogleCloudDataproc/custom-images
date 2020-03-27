67efe4fe2d2acffbea3d11f94d2d12024144d44d
Alex Merenda <alexm@moove.ai>
2020-03-27 16:06:55 -0400

    adds scrit to intall via conda forge

--- boot_new_qgis_conda_forge.sh
+++ boot_new_qgis_conda_forge.sh
@@ -13,11 +13,7 @@ conda update -n base -c defaults conda
 
 conda install python==3.6.8
 
-wget -q https://anaconda.org/conda-forge/qgis/3.12.1/download/linux-64/qgis-3.12.1-py36h77e4444_2.tar.bz2 -O /tmp/qgis-3.12.1-py36h77e4444_2.tar.bz2
-conda install --offline --use-local /tmp/qgis-3.8.3-py36hee8cbbe_0.tar.bz2
-
-wget -q https://anaconda.org/conda-forge/libspatialindex/1.9.3/download/win-64/libspatialindex-1.9.3-he025d50_3.tar.bz2 -O /tmp/libspatialindex-1.9.3-he025d50_3.tar.bz2
-conda install --offline --use-local /tmp/libspatialindex-1.9.3-he025d50_3.tar.bz2
+conda install qgis==3.12.1 --channel conda-forge
 
 conda install jupyterlab
 echo "source /etc/profile.d/effective-python.sh" >> /etc/bash.bashrc
---
+++ createImage-conda-forge.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sed -i '.bak' 's/GITHUB_OAUTH_TOKEN/9912a49487b845d3755fe2f92df8a5f05fe0639c/g' boot_new.sh
+
+python generate_custom_image.py \
+--image-name dataproc-custom-1-4-5-qgis-forge-$(date +%Y%m%d%H%M) \
+--dataproc-version 1.4.25-debian9 \
+--customization-script $(pwd)/boot_new_qgis_conda_forge.sh \
+--zone us-central1-b \
+--gcs-bucket gs://moove-dataproc-custom \
+--disk-size 100  \
+--machine-type n1-standard-8
+
+git checkout -- boot_new_qgis_conda_forge.sh
