0814824b33a7230bcc5a3c21d513d3f26092aaaf
Alex Merenda <alexm@moove.ai>
2020-03-29 18:28:31 -0400

    updates create image script

--- createImage.sh
+++ createImage.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
-sed -i '.bak' "s/GITHUB_OAUTH_TOKEN/$GITHUB_OAUTH_TOKEN/g" boot_new.sh
+if [ "$#" -ne 1 ]; then
+    echo "Please enter the name of the boot script"
+    exit 1
+fi
+
+BOOT_SCRIPT=$1
+
+if ! test -f ${BOOT_SCRIPT}; then
+    echo "please enter a valid boot script"
+    exit 1
+fi
+
+echo "Boot script: ${BOOT_SCRIPT}"
+
+sed -i '.bak' "s/GITHUB_OAUTH_TOKEN/$GITHUB_OAUTH_TOKEN/g" ${BOOT_SCRIPT}
 
 python generate_custom_image.py \
 --image-name dataproc-custom-1-4-5-anaconda-$(date +%Y%m%d%H%M) \
 --dataproc-version 1.4.25-debian9 \
---customization-script $(pwd)/boot_new.sh \
+--customization-script $(pwd)/${BOOT_SCRIPT} \
 --zone us-central1-b \
 --gcs-bucket gs://moove-dataproc-custom \
 --disk-size 100  \
 --machine-type n1-standard-8
 
-git checkout -- boot_new.sh
+git checkout -- ${BOOT_SCRIPT}
