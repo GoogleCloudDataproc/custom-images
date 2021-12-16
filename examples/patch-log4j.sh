8ecd6ee0839ecf918678bb3df170efa48f11a94e
Dagang Wei <functicons@gmail.com>
2021-12-16 15:26:51 -0800

    Fix regex to include jars without subminor version

--- examples/patch-log4j.sh
+++ examples/patch-log4j.sh
@@ -6,9 +6,9 @@ set -euxo pipefail
 # for CVE-2021-44228 in Dataproc custom images.
 
 function main() {
-  echo "Searching log4j jars of version in [2.0.0, 2.16.0)..."
+  echo "Searching for log4j jars of version [2.0.0, 2.16.0)..."
   local -a jars
-  mapfile -t jars < <(find / -regextype egrep -regex ".*/log4j-core-2\.([0-9]|1[0-5])\.[0-9]+\.jar$" || true)
+  mapfile -t jars < <(find / -regextype egrep -regex ".*/log4j-core-2\.([0-9]|1[0-5])(\.[0-9]+)?\.jar$" || true)
   echo "Found ${#jars[@]} jars"
   for jar in "${jars[@]}"; do
    echo "Patching ${jar}"
