2fa7b81870c905d44dbd88970f21def0f989d834
C.J. Collier <cjac@google.com>
2024-10-07 11:50:21 -0700

    revert change to key pair name

--- examples/secure-boot/create-key-pair.sh
+++ examples/secure-boot/create-key-pair.sh
@@ -25,7 +25,7 @@ set -ex
 
 # https://wiki.archlinux.org/title/Unified_Extensible_Firmware_Interface/Secure_Boot#Creating_keys
 
-ITERATION=047
+ITERATION=042
 
 CURRENT_PROJECT_ID="$(gcloud config get project)"
 if [[ -z "${CURRENT_PROJECT_ID}" ]]; then
