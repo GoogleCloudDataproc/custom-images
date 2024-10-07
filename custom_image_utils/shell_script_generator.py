cdb3f361225b323505f35ca8917f644df468929c
C.J. Collier <cjac@google.com>
2024-10-07 12:21:40 -0700

    deleting install image after customization instance has been started

--- custom_image_utils/shell_script_generator.py
+++ custom_image_utils/shell_script_generator.py
@@ -120,6 +120,11 @@ function main() {{
 
   touch /tmp/{run_id}/vm_created
 
+  # clean up intermediate install image
+  gcloud compute images delete {image_name}-install \
+    --project={project_id} \
+    --zone={zone} -q
+
   echo 'Waiting for customization script to finish and VM shutdown.'
   gcloud compute instances tail-serial-port-output {image_name}-install \
       --project={project_id} \
@@ -148,6 +153,7 @@ function main() {{
     --source-disk={image_name}-install \
     {storage_location_flag} \
     --family={family}
+
   touch /tmp/{run_id}/image_created
 }}
 
